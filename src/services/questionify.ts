343ebce2eb4bcc921f3919049852dffe3c808147
Bryson Lindy <bslindy@gmail.com>
2025-11-19 15:45:21 -0800

    made prompt less creative

--- src/services/questionify.ts
+++ src/services/questionify.ts
@@ -24,7 +24,7 @@ export async function generateQuestions(
     questions for the "question" field and provide 1 correct option and 3 incorrect options. Which option is correct should be random. If there is a 
     single question requested, the correct field should be 0, 1, 2, or 3. Not just 0. If there are multiple questions requested, the correct field should
     be random for EACH question. So we should expect if 4 questions are requested that one will have "correct": 1, another "correct": 2, etc. This is very
-    important as the application is useless if all the answers are always 0. DO NOT IGNORE THESE INSTRUCTIONS.
+    important as the application is useless if all the answers are always 0.
 {
     "questions": [
         {
@@ -41,7 +41,7 @@ ${input}`;
     const result = await client.chat.completions.create({
         model: 'deepseek-chat',
         messages: [{ role: 'user', content: prompt }],
-        temperature: 6.0
+        temperature: 0.0
     });
 
     const response = result.choices[0].message.content;
